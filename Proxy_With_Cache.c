400a1c57dd886dfe12a0f6a3e8a7ee8174808acc
Vinaykumar <vinaykumarj21@gmail.com>
2025-01-24 19:58:57 +0530

    Connecting to remote and handle HTTP Req

--- Proxy_With_Cache.c
+++ Proxy_With_Cache.c
@@ -93,3 +93,141 @@ int sendErrorMessage(int socket, int status_code)
 	}
 	return 1;
 }
+
+int connectRemoteServer(char* host_addr, int port_num)
+{
+	// Creating Socket for remote server ---------------------------
+
+	int remoteSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+	if( remoteSocket < 0)
+	{
+		printf("Error in Creating Socket.\n");
+		return -1;
+	}
+	
+	// Get host by the name or ip address provided
+
+	struct hostent *host = gethostbyname(host_addr);	
+	if(host == NULL)
+	{
+		fprintf(stderr, "No such host exists.\n");	
+		return -1;
+	}
+
+	// inserts ip address and port number of host in struct `server_addr`
+	struct sockaddr_in server_addr;
+
+	bzero((char*)&server_addr, sizeof(server_addr));
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(port_num);
+
+	bcopy((char *)host->h_addr,(char *)&server_addr.sin_addr.s_addr,host->h_length);
+
+	// Connect to Remote server ----------------------------------------------------
+
+	if( connect(remoteSocket, (struct sockaddr*)&server_addr, (socklen_t)sizeof(server_addr)) < 0 )
+	{
+		fprintf(stderr, "Error in connecting !\n"); 
+		return -1;
+	}
+	// free(host_addr);
+	return remoteSocket;
+}
+
+
+int handle_request(int clientSocket, ParsedRequest *request, char *tempReq)
+{
+	char *buf = (char*)malloc(sizeof(char)*MAX_BYTES);
+	strcpy(buf, "GET ");
+	strcat(buf, request->path);
+	strcat(buf, " ");
+	strcat(buf, request->version);
+	strcat(buf, "\r\n");
+
+	size_t len = strlen(buf);
+
+	if (ParsedHeader_set(request, "Connection", "close") < 0){
+		printf("set header key not work\n");
+	}
+
+	if(ParsedHeader_get(request, "Host") == NULL)
+	{
+		if(ParsedHeader_set(request, "Host", request->host) < 0){
+			printf("Set \"Host\" header key not working\n");
+		}
+	}
+
+	if (ParsedRequest_unparse_headers(request, buf + len, (size_t)MAX_BYTES - len) < 0) {
+		printf("unparse failed\n");
+		//return -1;				// If this happens Still try to send request without header
+	}
+
+	int server_port = 80;				// Default Remote Server Port
+	if(request->port != NULL)
+		server_port = atoi(request->port);
+
+	int remoteSocketID = connectRemoteServer(request->host, server_port);
+
+	if(remoteSocketID < 0)
+		return -1;
+
+	int bytes_send = send(remoteSocketID, buf, strlen(buf), 0);
+
+	bzero(buf, MAX_BYTES);
+
+	bytes_send = recv(remoteSocketID, buf, MAX_BYTES-1, 0);
+	char *temp_buffer = (char*)malloc(sizeof(char)*MAX_BYTES); //temp buffer
+	int temp_buffer_size = MAX_BYTES;
+	int temp_buffer_index = 0;
+
+	while(bytes_send > 0)
+	{
+		bytes_send = send(clientSocket, buf, bytes_send, 0);
+		
+		for(int i=0;i<bytes_send/sizeof(char);i++){
+			temp_buffer[temp_buffer_index] = buf[i];
+			// printf("%c",buf[i]); // Response Printing
+			temp_buffer_index++;
+		}
+		temp_buffer_size += MAX_BYTES;
+		temp_buffer=(char*)realloc(temp_buffer,temp_buffer_size);
+
+		if(bytes_send < 0)
+		{
+			perror("Error in sending data to client socket.\n");
+			break;
+		}
+		bzero(buf, MAX_BYTES);
+
+		bytes_send = recv(remoteSocketID, buf, MAX_BYTES-1, 0);
+
+	} 
+	temp_buffer[temp_buffer_index]='\0';
+	free(buf);
+	add_cache_element(temp_buffer, strlen(temp_buffer), tempReq);
+	printf("Done\n");
+	free(temp_buffer);
+	
+	
+ 	close(remoteSocketID);
+	return 0;
+}
+
+int checkHTTPversion(char *msg)
+{
+	int version = -1;
+
+	if(strncmp(msg, "HTTP/1.1", 8) == 0)
+	{
+		version = 1;
+	}
+	else if(strncmp(msg, "HTTP/1.0", 8) == 0)			
+	{
+		version = 1;										// Handling this similar to version 1.1
+	}
+	else
+		version = -1;
+
+	return version;
+}
