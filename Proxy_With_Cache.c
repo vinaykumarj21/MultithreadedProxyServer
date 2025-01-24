cce0dbfac55dbbed89143704579f4459dc7a4e34
Vinaykumar <vinaykumarj21@gmail.com>
2025-01-24 19:55:30 +0530

    Added sendError function

--- Proxy_With_Cache.c
+++ Proxy_With_Cache.c
@@ -46,3 +46,50 @@ pthread_mutex_t lock;               //lock is used for locking the cache
 
 cache_element* head;                //pointer to the cache
 int cache_size;             //cache_size denotes the current size of the cache
+
+int sendErrorMessage(int socket, int status_code)
+{
+	char str[1024];
+	char currentTime[50];
+	time_t now = time(0);
+
+	struct tm data = *gmtime(&now);
+	strftime(currentTime,sizeof(currentTime),"%a, %d %b %Y %H:%M:%S %Z", &data);
+
+	switch(status_code)
+	{
+		case 400: snprintf(str, sizeof(str), "HTTP/1.1 400 Bad Request\r\nContent-Length: 95\r\nConnection: keep-alive\r\nContent-Type: text/html\r\nDate: %s\r\nServer: VaibhavN/14785\r\n\r\n<HTML><HEAD><TITLE>400 Bad Request</TITLE></HEAD>\n<BODY><H1>400 Bad Rqeuest</H1>\n</BODY></HTML>", currentTime);
+				  printf("400 Bad Request\n");
+				  send(socket, str, strlen(str), 0);
+				  break;
+
+		case 403: snprintf(str, sizeof(str), "HTTP/1.1 403 Forbidden\r\nContent-Length: 112\r\nContent-Type: text/html\r\nConnection: keep-alive\r\nDate: %s\r\nServer: VaibhavN/14785\r\n\r\n<HTML><HEAD><TITLE>403 Forbidden</TITLE></HEAD>\n<BODY><H1>403 Forbidden</H1><br>Permission Denied\n</BODY></HTML>", currentTime);
+				  printf("403 Forbidden\n");
+				  send(socket, str, strlen(str), 0);
+				  break;
+
+		case 404: snprintf(str, sizeof(str), "HTTP/1.1 404 Not Found\r\nContent-Length: 91\r\nContent-Type: text/html\r\nConnection: keep-alive\r\nDate: %s\r\nServer: VaibhavN/14785\r\n\r\n<HTML><HEAD><TITLE>404 Not Found</TITLE></HEAD>\n<BODY><H1>404 Not Found</H1>\n</BODY></HTML>", currentTime);
+				  printf("404 Not Found\n");
+				  send(socket, str, strlen(str), 0);
+				  break;
+
+		case 500: snprintf(str, sizeof(str), "HTTP/1.1 500 Internal Server Error\r\nContent-Length: 115\r\nConnection: keep-alive\r\nContent-Type: text/html\r\nDate: %s\r\nServer: VaibhavN/14785\r\n\r\n<HTML><HEAD><TITLE>500 Internal Server Error</TITLE></HEAD>\n<BODY><H1>500 Internal Server Error</H1>\n</BODY></HTML>", currentTime);
+				  //printf("500 Internal Server Error\n");
+				  send(socket, str, strlen(str), 0);
+				  break;
+
+		case 501: snprintf(str, sizeof(str), "HTTP/1.1 501 Not Implemented\r\nContent-Length: 103\r\nConnection: keep-alive\r\nContent-Type: text/html\r\nDate: %s\r\nServer: VaibhavN/14785\r\n\r\n<HTML><HEAD><TITLE>404 Not Implemented</TITLE></HEAD>\n<BODY><H1>501 Not Implemented</H1>\n</BODY></HTML>", currentTime);
+				  printf("501 Not Implemented\n");
+				  send(socket, str, strlen(str), 0);
+				  break;
+
+		case 505: snprintf(str, sizeof(str), "HTTP/1.1 505 HTTP Version Not Supported\r\nContent-Length: 125\r\nConnection: keep-alive\r\nContent-Type: text/html\r\nDate: %s\r\nServer: VaibhavN/14785\r\n\r\n<HTML><HEAD><TITLE>505 HTTP Version Not Supported</TITLE></HEAD>\n<BODY><H1>505 HTTP Version Not Supported</H1>\n</BODY></HTML>", currentTime);
+				  printf("505 HTTP Version Not Supported\n");
+				  send(socket, str, strlen(str), 0);
+				  break;
+
+		default:  return -1;
+
+	}
+	return 1;
+}
